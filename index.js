86c8ffa37930ca844108a9f550dfe2b1e8bccd0d
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-03 08:14:23 +0600

    server updated

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 const chef = require('./data/chef.json');
 
+
 app.use(cors())
 
 app.get('/', (req, res)=>{
@@ -14,6 +15,15 @@ app.get('/', (req, res)=>{
 app.get('/chef', (req, res)=>{
     res.send(chef);
 })
+
+
+  
+  app.get("/recipes/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedChef = chef.find((c) => parseInt(c.id) === id);
+    res.send(selectedChef);
+  });
+
 
 app.listen(port, ()=>{
     console.log(`Quince api is running on port: ${port}`);
